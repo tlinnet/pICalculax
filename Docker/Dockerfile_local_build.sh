54b1fd4fcd642d815d2832dbf421b1db0a269826
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-12-22 13:04:27 +0100

    Working on Dockerfile to build RDKit

--- Docker/Dockerfile_local_build.sh
+++ Docker/Dockerfile_local_build.sh
@@ -2,5 +2,9 @@
 
 # Build user setup
 docker build -t $USER/picalculax:01_setup -f Dockerfile_local .
+# Start Jupyter Notebook
 alias pi='docker run -ti --rm -p 8888:8888 -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:01_setup'
+# Start in bash
+alias pib='pi bash'
+# Execute in running image
 alias pie='docker exec -it picalculax'
