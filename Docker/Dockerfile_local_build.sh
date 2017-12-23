f1a02163d1ec00224bd6a44daaec75d4359c52ae
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-12-23 21:30:35 +0100

    Updating build files

--- Docker/Dockerfile_local_build.sh
+++ Docker/Dockerfile_local_build.sh
@@ -4,7 +4,10 @@
 docker build -t $USER/picalculax:01_setup -f Dockerfile_local .
 # Start Jupyter Notebook
 alias pi='docker run -ti --rm -p 8888:8888 -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:01_setup'
+#alias pi='docker run -ti --rm -p 8888:8888 -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.0.1|cut -d" " -f2):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:01_setup'
+
 # Start in bash
 alias pib='pi bash'
+
 # Execute in running image
 alias pie='docker exec -it picalculax'
--- Dockerfile_build.sh
+++ Dockerfile_build.sh
@@ -3,8 +3,11 @@
 # Build user setup
 docker build -t $USER/picalculax:02_picalculax -f Dockerfile .
 # Start Jupyter Notebook
-alias pi='docker run -ti --rm -p 8888:8888 -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:02_picalculax'
+#alias pi='docker run -ti --rm -p 8888:8888 -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:02_picalculax'
+alias pi='docker run -ti --rm -p 8888:8888 -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.0.1|cut -d" " -f2):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/jovyan/work --name picalculax $USER/picalculax:02_picalculax'
+
 # Start in bash
 alias pib='pi bash'
+
 # Execute in running image
 alias pie='docker exec -it picalculax'
